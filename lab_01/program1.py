01a3930509a86eac2eee3719895b7eea4848df52
ef1301 <fangemily60@gmail.com>
2018-09-06 16:42:31 -0400

    front_back+print

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -82,6 +82,13 @@ def missing_char(str, n):
   back = str[n+1:]
   return front + back
 print(missing_char("kitten", 4))
+#Warmup-1:front_back
+def front_back(str):
+  if len(str) <= 1:
+    return str
+  mid=str[1:len(str)-1]
+  return str[len(str)-1] + mid +str[0]
+print(front_back("code"))
 #String-1:hello_name
 def hello_name(name):
   return "Hello "+ name + "!"
