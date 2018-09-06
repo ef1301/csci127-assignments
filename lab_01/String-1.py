efdbae4141ed4007da89f24428b0ca59b148625c
ef1301 <fangemily60@gmail.com>
2018-09-06 10:02:50 -0400

    warmup monkey_trouble

--- lab_01/String-1.py
+++ lab_01/String-1.py
@@ -4,25 +4,36 @@ def sleep_in(weekday, vacation):
     return True
   else:
     return False
+#Warmup-1:monkey_trouble
+def monkey_trouble(a_smile, b_smile):
+  if a_smile and b_smile:
+    return True
+  if not a_smile and not b_smile:
+    return True
+  return False
+print(monkey_trouble(True, False))
+print(monkey_trouble(False, True))
+print(monkey_trouble(False, False))
+print(monkey_trouble(True, True))
 #Warmup-1: sum_double
 def sum_double(a, b):
   sum = a + b
   if a == b:
     sum = sum*2
   return sum
-#hello_name
+#String-1:hello_name
 def hello_name(name):
   return "Hello "+ name + "!"
 print(hello_name("Bob"))
 print(hello_name("Alice"))
 print(hello_name("X"))
-#make abba
+#String-1make abba
 def make_abba(a, b):
   return a + b + b + a
 print(make_abba('Hi','Bye'))
 print(make_abba('Yo','Alice'))
 print(make_abba('What','Up'))
-#make_tags
+#String-1:make_tags
 def make_tags(tag, word):
   return "<" + tag + ">" + word + "</" + tag + ">"
 print(make_tags('i', 'Yay'))
