a6c51858c2cabb4cb1bf7a82818d7a6281b90f01
ef1301 <fangemily60@gmail.com>
2018-09-13 13:50:45 -0400

    string1 make_tags & make_out_word

--- hw_01/program.py
+++ hw_01/program.py
@@ -1,3 +1,4 @@
+#CAPITALIZE
 def capitalize(name):
     """
     input: name --> a string in the form "first last"
@@ -12,7 +13,7 @@ def capitalize(name):
     return first + " " + last
 print(capitalize("beth sanders"))
     
-    
+#INIT    
 def init(name):
     """
     Input: name --> a string in the form "first last"
@@ -27,7 +28,7 @@ def init(name):
     return first + ". " + last
 print(init("daniel erwing"))
 
-
+#PART_PIG_LATIN
 def part_pig_latin(name):
     """
     Input: A string that is a single lower case word
@@ -42,4 +43,19 @@ def part_pig_latin(name):
 print(part_pig_latin("Hello"))
 print(part_pig_latin("Mary"))
 
+#MAKE_OUT_WORD
+def make_out_word(out, word): #defined function with input out and word
+  if len(out)==4: #if the length of the out input is 4
+    first=out[:2] #first is assigned to the first two charaters
+    last=out[2:] #last is assigned from the second character to the last
+  return first + word + last #return the first 2 chars of out +word +the rest of the out input after the 2nd char
+print(make_out_word("<<>>","Same"))
+
+#MAKE_TAGS
+def make_tags(tag, word): #defined function with tag and word inputs
+  return "<" + tag + ">" + word + "</" + tag + ">" # return tags between "<" & ">" and "</" & ">"
+    #in between the tags and their pairs of "<" and ">" word input occurs
+print(make_tags('i', 'Yay'))
+print(make_tags('i', 'Hello'))
+print(make_tags('cite', 'Yay'))
 
