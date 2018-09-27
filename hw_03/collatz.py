cb04a440df89e217bd4ad2c8544428f9de64f767
ef1301 <fangemily60@gmail.com>
2018-09-27 13:17:01 -0400

    change to return

--- hw_03/collatz.py
+++ hw_03/collatz.py
@@ -9,6 +9,6 @@ def collatz(a):
             a = 3*a + 1
             print(a)
             c = c + 1
-    print("The input has ran ", c, " times in this code")
+    return("The input has ran ", c, " times in this code")
 print(collatz(7))
 print(collatz(6))
