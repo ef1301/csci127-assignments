2a09fd2c28b11f04a54813c88f88c6d8f0a2171a
ef1301 <fangemily60@gmail.com>
2018-10-04 08:02:08 -0400

    hw_04

---
+++ hw_04/lists.py
@@ -0,0 +1,18 @@
+import random
+
+def build_random_list(size,max_value):
+    """
+    Parameters:
+      size : the number of elements in the lsit
+      max_value : the max random value to put in the list
+    """
+    l = [] # start with an empty list
+
+    # make a loop that counts up to size
+    i = 0
+    while i < size:
+        l.append(random.randrange(0,max_value))
+        # we could have written this instead of the above line:
+        # l = l + [random.randrange(0,max_value)]
+        i = i + 1
+    return l
