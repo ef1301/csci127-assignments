25f265110e11c8d4eebb74b74613e776446a46dc
ef1301 <fangemily60@gmail.com>
2018-10-11 14:00:16 -0400

    hw_05 w/ append

--- hw_04/lists.py
+++ hw_04/lists.py
@@ -1,10 +1,67 @@
 #Emily Fang & Darren Liang
-import random
+##import random
+##
+##def build_random_list(size,max_value):
+##    """
+##    Parameters:
+##      size : the number of elements in the lsit
+##      max_value : the max random value to put in the list
+##    """
+##    l = [] # start with an empty list
+##
+a##    # make a loop that counts up to size
+##    i = 0
+##    while i < size:
+##        l.append(random.randrange(0,max_value))
+##        # we could have written this instead of the above line:
+##        # l = l + [random.randrange(0,max_value)]
+##        i = i + 1
+##    return l
+##print(build_random_list(3,100))
+##
+##l = [3, 2, 3]
+##
+##def locate(l,value):
+##    i = 0
+##    while i < len(l):
+##        if l[i] == value:
+##            return i
+##        else:
+##            i += 1
+##    return -1
+##print(locate(l,3))
+##print(locate(l,5))
+##
+##def count(l,value):
+##    counter = 0
+##    i = 0
+##    while i < len(l):
+##        if l[i] == value:
+##            counter += 1
+##        i += 1
+##    return counter
+##print(count(l,2))
+##
+##def reverse(l):
+##    return l[::-1]
+##print(reverse(l))
+##
+##def isIncreasing(l):
+##    for i in range(len(l)-1):
+##        if l[i] > l[i+1]:
+##            return False
+##    return True
+##print(isIncreasing(l))
+##
+##def palindrome(l):
+##    return reverse(l) == 1
+##print(palindrome(l))
 
+import random
 def build_random_list(size,max_value):
     """
     Parameters:
-      size : the number of elements in the lsit
+      size : the number of elements in the list
       max_value : the max random value to put in the list
     """
     l = [] # start with an empty list
@@ -17,42 +74,57 @@ def build_random_list(size,max_value):
         # l = l + [random.randrange(0,max_value)]
         i = i + 1
     return l
-print(build_random_list(3,100))
 
-l = [3, 2, 3]
-
-def locate(l,value):
+def locate(l, value): # returns the first appearance
     i = 0
-    while i < len(l):
+    while i < len(l): # go through the list until you find the value
         if l[i] == value:
-            return i
+            return i # the value has been found
         else:
             i += 1
-    return -1
-print(locate(l,3))
-print(locate(l,5))
+    return -1 # the loop did not find the value, so it should return -1
 
-def count(l,value):
-    counter = 0
+def count(l, value):
     i = 0
-    while i < len(l):
+    counter = 0
+    while i < len(l): # loop through the entire list
         if l[i] == value:
-            counter += 1
+            counter += 1 # add 1 to counter when a value is found
         i += 1
-    return counter
-print(count(l,2))
+    return counter # number of times the value appears
 
 def reverse(l):
-    return l[::-1]
-print(reverse(l))
+    return l[::-1] # return the entire list, but in every -1 step. aka backwards
 
 def isIncreasing(l):
-    for i in range(len(l)-1):
-        if l[i] > l[i+1]:
+    i = 0
+    while i < len(l) - 1: # length needs to be minus 1, because 
+        if l[i] < l[i + 1]: # l[i + 1] will be the last number to be checked
+            i += 1
+        else:
             return False
     return True
-print(isIncreasing(l))
 
-def palindrome(l):
-    return reverse(l) == 1
-print(palindrome(l))
+def palindrome(l): # palindrome means the lists should be the same when reversed
+    return l == reverse(l)
+    
+
+l = [1, 3, 5, 7, 11, 13, 17]
+l2 = [7, 7, 4, 1 , 0, 0, 1, 4, 7, 7]
+
+print(build_random_list(10, 100))
+
+print(locate(l, 4)) # should return -1
+print(locate(l2, 1)) # should return 3
+
+print(count(l, 3)) # should return 1
+print(count(l2, 7)) # should return 4
+
+print(reverse(l))
+print(reverse(l2))
+
+print(isIncreasing(l)) # should return True
+print(isIncreasing(l2)) # should return False
+
+print(palindrome(l)) # should return False
+print(palindrome(l2)) # should return True
