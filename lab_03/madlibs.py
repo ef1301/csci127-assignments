9f0e261ad529f843a5a29212e847ed453cf6e63b
ef1301 <fangemily60@gmail.com>
2018-10-18 08:52:07 -0400

    adjectives

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -1,9 +1,11 @@
 #EMILY FANG AND RACHIEL TIEU
 import random
-VERBS = ['ate','walked', 'ran', 'skipped'] # verbs list
+ADJECTIVES = ["amazing", "delicious", "determined"]
+VERBS = ['ate','walked', 'fought', 'skipped'] # verbs list
 NOUNS = ['dog','sandwich','cat','food','frog'] # nouns list
 HEROS = ['Spiderman', 'Hulk', 'Flash'] #heros list
-sentence = "<HERO> <VERB> in the <NOUN> and then <HERO> <VERB> <NOUN> later." #sentence
+sentence = "<HERO> <VERB> the <ADJECTIVE> <NOUN> and then <HERO> <VERB> the <NOUN> later." #sentence
+
 def madlib(s):
     new_list = [] #empty list that will have strings added to it and returned
     H_random = random.choice(HEROS) # a hero is randomly taken from the heros list, is assigned to H_assign, and remains that value
@@ -12,6 +14,8 @@ def madlib(s):
             new_list.append(random.choice(NOUNS)) #a noun will be randomly taken from the NOUNS list and appended to the new list
         elif item == "<VERB>": # if the item is <VERB>
             new_list.append(random.choice(VERBS)) #a verb will be randomly taken from the VERBS list and appended to the new list
+        elif item == "<ADJECTIVE>": # if the item is <NOUN>
+                new_list.append(random.choice(ADJECTIVES))
         elif item == "<HERO>": # if the item is <HERO>
             new_list.append(H_random) # the H_random value initially assigned to H_random will be appended to the new list
         else: # otherwise
