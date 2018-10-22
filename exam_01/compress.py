be9f526b0b720f6168fb430debdfebc9e8339eda
ef1301 <fangemily60@gmail.com>
2018-10-22 08:42:05 -0400

    compress fixed

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -2,11 +2,19 @@ def compress_word(w):
     new = []
     vowels = ["a", "e", "i", "o", "u"]
     lower = w.lower()
-    for i in lower:
-        if not i in vowels:
-            new.append(i)
-            join = "".join(new)
-    return join
+    if "w" in w:
+        new.append(w[:1])
+        for i in w[1:]:
+            if not i in vowels:
+                new.append(i)
+                join = "".join(new)
+        return join
+    else:
+        for i in w:
+            if not i in vowels:
+                new.append(i)
+                join = "".join(new)
+        return join
 print(compress_word("halloween"))
 print(compress_word("Special"))
 print(compress_word("apple"))
