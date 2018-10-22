c58112d209e15a3fe71768472284e8068b61d1e7
ef1301 <fangemily60@gmail.com>
2018-10-22 09:41:19 -0400

    updated README.md

--- exam_01/cake.py
+++ exam_01/cake.py
@@ -1,6 +1,4 @@
 def divide(A,B,u):
-    fraction = A/B
-    ratio = fraction.as_integer_ratio()
-    people = ratio[1]
-    return people
+    fraction = u // (A/B)
+    return int(fraction)
 print(divide(5,10,1))
