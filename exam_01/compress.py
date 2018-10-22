0e2ac641b0cd74d8aa73cd2f27088f2c3a028022
ef1301 <fangemily60@gmail.com>
2018-10-22 09:06:17 -0400

    compress no w

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -7,7 +7,6 @@ def compress_word(w):
         if not i in vowels:
             new.append(i)
             join = "".join(new)
-
     return join
 print(compress_word("halloween"))
 print(compress_word("Special"))
