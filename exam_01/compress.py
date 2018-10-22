55469a4486f8c1ac07564a41a1585c790bc14a05
ef1301 <fangemily60@gmail.com>
2018-10-22 09:08:50 -0400

    compress

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -2,8 +2,8 @@ def compress_word(w):
     new = []
     vowels = ["a", "e", "i", "o", "u"]
     lower = w.lower()
-    
-    for i in w:
+    new.append(w[0])
+    for i in w[1:]:
         if not i in vowels:
             new.append(i)
             join = "".join(new)
@@ -12,13 +12,3 @@ print(compress_word("halloween"))
 print(compress_word("Special"))
 print(compress_word("apple"))
 
-
-def sentence(line):
-    new = []
-    sep = line.split(" ")
-    for str in sep:
-        word = compress_word(str)
-        new.append(word)
-    return " ".join(new)
-print(sentence("I like to eat pie"))
-print(sentence("who is there"))
