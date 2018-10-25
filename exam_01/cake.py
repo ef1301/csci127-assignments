b435e324872ef3c8e74d176a499b108bccc806fc
ef1301 <fangemily60@gmail.com>
2018-10-25 08:38:23 -0400

    classnotes

--- exam_01/cake.py
+++ exam_01/cake.py
@@ -3,3 +3,4 @@ def divide(A,B,u):
     return int(fraction)
 print(divide(5,10,1))
 print(divide(6,15,2))
+#make sure to handle different cases
--- exam_01/compress.py
+++ exam_01/compress.py
@@ -1,15 +1,21 @@
 def compress_word(w):
     new = []
-    vowels = ["a", "e", "i", "o", "u"]
-    new.append(w[:1])
+    vowels = "aeiouAEIOU"
+    #rest = word[1:]
+# OR vowels = ["a","e","i","o","u","A",..."U"]
+    new.append(w[0]) # first letter
     for i in w[1:]:
         if not i in vowels:
             new.append(i)
+            #if letter not in vowels:
+                #new_rest = new_rest + letter
     return "".join(new)
+    #return first + new_rest
 print(compress_word("halloween"))
 print(compress_word("Special"))
 print(compress_word("apple"))
-
+print("Testing compress_word")
+print("apple", compress_word("apple"))
 
 def sentence(line):
     new = []
