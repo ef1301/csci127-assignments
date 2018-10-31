6259a841f27b035f3a6bbf9979dbcd61674bec23
ef1301 <fangemily60@gmail.com>
2018-10-31 13:24:23 -0400

    done? testing begins

--- lab_04/lady.py
+++ lab_04/lady.py
@@ -8,39 +8,41 @@ def count(str):
     for i in str: # runs through each item in the parameter string
         if i != "_": # as long as i is not underscore
             if i not in lady: # if the i is not in the lady bucket
-                lady.append(i) # add i to the lady bucket 
+                lady.append(i) # add i to the lady bucket
                 counter.append(1) # add 1 t the counter bucket to begin the count and create the range
-#                print(lady) 
+#                print(lady)
 #                print(counter)
             else: #otherwise
                 counter[lady.index(i)] += 1 #adds 1 to each occurance of i at the index of i
 #                print(counter)
     return counter
-##    
-##def happy(n,b): #happiness check
-##    if len(b) == 0: #there can't not be anything in the string
-##        return False
-##    elif len(b) == 1 and b[1] == "_": #if there's only "_" in the string 
-##        return True
-##    elif "_" not in b: # if "_" isn't in b
-##        return False
-##    for i in b:
-##            position = p.index(i)
-            
-            
-        
 
-##def happyLadybugs(n,b):
-##    n = len(b)
+def happy(games,str): #happiness check
+    if games == 0: #there can't not be anything in the string
+        return False
+    elif games == 1 and str[0] == "_": #if there's only "_" in the string
+        return True
+    elif "_" in str: # if "_" isn't in b
+        check = count(str)
+        for i in check:
+            if i < 2:
+                return False
+            else:
+                return True
+    for i in range(0,games):
+        if i>0 and str[i] != str[i-1]:
+            return False
+        if str[i] != str[i+1]:
+            print(i)
+            return False
+    return True
 
-g = ["RBY_YBR", "AABBC", "DD__FQ_QQF"]
-def test(list):
-    """
-TESTING TESTING 123
-    """
-    for item in list:
-        print(count(item))
-#print(test(g))
-#print(count(g))
-##print(happy(5,g))
-##print(happyLadybugs(g))
+def happyLadybug(games,str):
+    if happy(games,str) == True: #if happy/True
+        pass
+    elif happy(games,str) != True:
+        return "NO"
+    return "YES"
+
+print(happy(6,"RBY_YBR"))
+print(happyLadybug(6,"RBY_YBR"))
