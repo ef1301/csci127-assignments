efbbaef8444d79a69dcbb2104133fab545ed3121
ef1301 <fangemily60@gmail.com>
2018-10-31 00:46:39 -0400

    counter

--- lab_04/apple.py
+++ lab_04/apple.py
@@ -10,8 +10,7 @@ def countApplesandOranges(s,t,a,b,m,n):
         posb = b + item
         if posb in range(s,t+1):
             sam_oranges += 1
-    print("Sam's apples: ", sam_apples)
-    print("Sam's oranges:", sam_oranges)
+    return "Sam's apples:", sam_apples ,"Sam's oranges:", sam_oranges
 
 
 s,t = [7,11]
@@ -21,3 +20,24 @@ apples = [-2,2,1]
 oranges = [5,-6]
 
 print(countApplesandOranges(s,t,a,b,m,n))
+
+##def countApplesandOranges(s,t,a,b,m,n):
+##    sam_apples = 0
+##    sam_oranges = 0
+##    
+##    for item in apples: #for each
+##        if a + item in range(s,t+1):
+##            sam_apples += 1
+##    for item in oranges:
+##        if b - item in range(s,t+1):
+##            sam_oranges += 1
+##    return "Sam's apples:", sam_apples ,"Sam's oranges:", sam_oranges
+##
+##
+##s,t = [7,11]
+##a,b = [5,15]
+##m,n = [3,2]
+##apples = [-2,2,1]
+##oranges = [5,-6]
+##
+##print(countApplesandOranges(s,t,a,b,m,n))
--- lab_04/lady.py
+++ lab_04/lady.py
@@ -1,2 +1,39 @@
-def happyLadybugs(b):
+def count(str):
+    """
+    Utilizes 'buckets' to store information: the different colors and how
+    many times they occur in a string.
+    """
+    lady = [] # stores the different color variables
+    counter = [] # stores the occurances of each color variable in the order of the lady bucket
+    for i in str: # runs through each item in the parameter string
+        if i != "_": # as long as i is not underscore
+            if i not in lady: # if the i is not in the lady bucket
+                lady.append(i) # add i to the lady bucket 
+                counter.append(1) # add 1 t the counter bucket to begin the count and create the range
+#                print(lady)
+#                print(counter)
+            else: #otherwise
+                counter[lady.index(i)] += 1
+                print(counter)
+    return counter
+##    
+##def happy(n,b): #happiness check
+##    if len(b) == 0: #there can't not be anything in the string
+##        return False
+##    elif len(b) == 1 and b[1] == "_": #if there's only "_" in the string 
+##        return True
+##    elif "_" not in b: # if "_" isn't in b
+##        return False
+##    for i in b:
+##            position = p.index(i)
+            
+            
+        
+
+##def happyLadybugs(n,b):
+##    n = len(b)
     
+g = "RBY_YBR"
+print(count(g))
+##print(happy(5,g))
+##print(happyLadybugs(g))
