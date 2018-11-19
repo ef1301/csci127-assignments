6f83a2a61d929968ec693259375399b013586d56
ef1301 <fangemily60@gmail.com>
2018-11-19 08:42:13 -0500

    acronym.py

--- exam_02/acronym.py
+++ exam_02/acronym.py
@@ -1,17 +1,22 @@
 def makeacronym(w):
     ac = []
     lower = w.lower()
-    print(lower)
+##    print(lower)
 ##    split = lower.split()
 ##    print(split)
     ac.append(lower[0])
-    for item in lower:
-        print(item)
-        if item == " ":
-            ac.append(lower[lower.index(item)+1])
-            print(ac)
+    for i in range(0,len(lower)):
+##        print(i)
+        if lower[i] == " ":
+            if lower[i+1] in "abcdefghijklmnopqrstuvwxyz":
+                ac.append(lower[i+1])
+##                print(ac)
         else:
             continue
     return "".join(ac)
 
 print(makeacronym("Laugh out Loud"))
+print(makeacronym("Read the ... fine manuel"))
+print(makeacronym("Rolling on Floor Laughing"))
+print(makeacronym("in my humble opinion"))
+print(makeacronym("In my not so humble opinion"))
