9125d11e169b902f643735dac9b625585e5670a8
ef1301 <fangemily60@gmail.com>
2018-11-19 09:25:17 -0500

    stuff

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,11 +1,15 @@
 def encode(input):
     list = []
-    count = 0
+    count = 1
     for i in range(1,len(input)):
         if input[i-1] == input[i]:
             count += 1
             result = [input[i],count]
+            print(result)
         elif input[i-1] != input[i]:
+            result = [input[i-1],count]
+            list.append(result)
+            print(list)
             count = 0
             for i in range(i,len(input)):
                 if input[i-1] == input[i]:
@@ -15,19 +19,19 @@ def encode(input):
     return list
 
 
-##def decode(d):
-##    str = []
-##    for i in d:
-##        val = d[i]
-##        reconstruct = val*i
-##        str.append(reconstruct)
-##    return "".join(str)
+def decode(d):
+    str = []
+    for i,num in d: 
+        reconstruct = i*num
+        str += reconstruct
+    return "".join(str)
+
 print(encode("aaaabb"))
 ##print(encode("bbbaaaabb"))
 ##print(encode("abcd"))
 ##print(encode("cbbbbdee"))
 
-##print(decode(encode("aaaabb")))
+print(decode(encode("aaaabb")))
 ##print(decode(encode("bbbaaaabb")))
 ##print(decode(encode("abcd")))
 ##print(decode(encode("cbbbbdee")))
