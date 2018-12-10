bfaef3ec91e4f34ba91090295e518a1154f05980
ef1301 <fangemily60@gmail.com>
2018-12-10 08:51:14 -0500

    moar tests

--- final_1/quad.cpp
+++ final_1/quad.cpp
@@ -11,7 +11,7 @@ double quadsolve(int a, int b, int c){
   double d;
   d = discriminant(a,b,c);
   if(d>=0){
-    return (-b+sqrt(d))/2*a;
+    return (-b+sqrt(d))/(2*a);
   }
   else{
     return 0;
@@ -19,12 +19,14 @@ double quadsolve(int a, int b, int c){
 }
 
 int main(){
-  std::cout << discriminant(1,2,3) << std::endl;
-  std::cout << discriminant(6,10,-1) << std::endl;
-  std::cout << discriminant(1,6,-14) << std::endl;
+  std::cout << "Your discriminant is " <<  discriminant(1,2,3) << std::endl;
+  std::cout << "Your discriminant is " << discriminant(6,10,-1) << std::endl;
+  std::cout << "Your discriminant is " << discriminant(1,6,-14) << std::endl;
+  std::cout << "Your discriminant is " << discriminant(8,14,-15) << std::endl;
   std::cout << quadsolve(1,2,3) << std::endl;
   std::cout << quadsolve(6,10,-1) << std::endl;
   std::cout << quadsolve(1,6,-14) << std::endl;
+  std::cout << quadsolve(8,14,-15) << std::endl;
   return 0;
 }
 
