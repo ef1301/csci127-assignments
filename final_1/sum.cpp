a368bad14444a01a2fceca8f22fcb42e1ee46bf2
ef1301 <fangemily60@gmail.com>
2018-12-10 08:33:28 -0500

    sum done

--- final_1/sum.cpp
+++ final_1/sum.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-int main(){
+int sumofsquares(int a, int b){
+  int sum;
+  while(a<b){
+    sum = 0;
+    for(a ; a<b+1 ; a = a+1){
+      int square = a*a;
+      sum = sum + square;
+    }
+    return sum;
+  }
+}
 
+int main(){
+  std::cout << sumofsquares(5,10) << std::endl;
+  return 0;
 }
