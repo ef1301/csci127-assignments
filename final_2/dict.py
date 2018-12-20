e9ec23c511a0511251892178f7db93d67dc4baa4
ef1301 <fangemily60@gmail.com>
2018-12-20 09:53:47 -0500

    3.1 and 3.2 done

--- final_2/dict.py
+++ final_2/dict.py
@@ -3,12 +3,22 @@ def addline(d,line):
     split = lower.split()
     for item in split:
         l = []
-        print(item)
+##        print(item)
         if item[0] in d:
             d[item[0]].append(item)
-            d[item[0]].append(l)
         else:
             l.append(item)
             d.setdefault(item[0],l)
     return d
 print(addline({},"Cats Birds donkey Dove"))
+
+def main(d,line):
+    i = 0
+    while i < 5:
+        addline(d,line)
+        i += 1
+    return d
+print(main({},"Cats Birds donkey Dove"))
+
+##def spellcheck(d,word):
+##    
