0732cb3651ed9a8930fa0a4db4891180b2901ecb
ef1301 <fangemily60@gmail.com>
2018-12-20 09:06:37 -0500

    got back to notpossesive

--- final_2/plural.py
+++ final_2/plural.py
@@ -9,5 +9,16 @@ def countPlurals(line):
     return count
 print(countPlurals("dogs cats bird tree"))
 print(countPlurals("desks chairs boxes head shoulders"))
+
 def notPossesive(line):
+    split = line.split()
+    count = 0
+    for item in split:
+        if item[len(item)-2:len(item)-1] == "'s":
+            continue
+        elif item[len(item)-1]== "s":
+            count += 1
+    return count
+print(notPossesive("dog's cats bird tree's"))
+print(notPossesive("desks chair's boxes head shoulder's"))
     
