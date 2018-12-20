4b5eb89432cffc101eeed0af688f8c7e2757a96b
ef1301 <fangemily60@gmail.com>
2018-12-20 10:32:04 -0500

    checking

--- final_2/plural.py
+++ final_2/plural.py
@@ -9,13 +9,13 @@ def countPlurals(line):
     return count
 print(countPlurals("dogs cats bird tree"))
 print(countPlurals("desks chairs boxes head shoulders"))
+print(countPlurals("zephyrs chair courses food"))
 
 def notPossesive(line):
     split = line.split()
     count = 0
     for item in split:
         if item[-1] == "s":
-##            print(item[-2:len(item)])
             if item[-2:len(item)] != "'s":
                 count += 1
         else:
@@ -23,4 +23,4 @@ def notPossesive(line):
     return count
 print(notPossesive("dog's cats bird tree's"))
 print(notPossesive("desks chair's boxes head shoulder's"))
-print(notPossesive("desk chair's boxes heads snake's"))
+print(notPossesive("desk chair's boxes heads boars snake's"))
