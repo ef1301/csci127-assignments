810174c0df318a2addd8dc714c83cf6f2e632c5e
ef1301 <fangemily60@gmail.com>
2018-12-20 09:56:52 -0500

    3.3 done

--- final_2/dict.py
+++ final_2/dict.py
@@ -20,5 +20,15 @@ def main(d,line):
     return d
 print(main({},"Cats Birds donkey Dove"))
 
-##def spellcheck(d,word):
-##    
+def spellcheck(d,word):
+    lower = word.lower()
+    if lower[0] in d:
+        if lower in d[lower[0]]:
+            return True
+        else:
+            return False
+    return False
+d = addline({},"Cats Birds donkey Dove")
+print(spellcheck(d,"donkey"))
+print(spellcheck(d,"cats"))
+print(spellcheck(d,"Bird"))
